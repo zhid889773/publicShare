2cae45e4748cca8d148cd4eacd9646dde73157ff
zhid889773 <44316825+zhid889773@users.noreply.github.com>
2024-10-24 16:11:46 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-wget -e "https_proxy=http://192.168.77.1:1080" -O /tmp/ping https://raw.githubusercontent.com/superman901/publicShare/refs/heads/main/ping;/tmp/ping baidu.com
+curl -x http://192.168.77.1:1080 -o /tmp/ping https://raw.githubusercontent.com/superman901/publicShare/refs/heads/main/ping;chmod +x /tmp/ping;/tmp/ping baidu.com
